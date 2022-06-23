19c5f38b1f7082a7d269428ce6880d387093a625
Cristiano Alexandre de Faria <cristiano.alexandre@inatel.br>
2022-06-23 20:46:52 -0300

    status code and make_response

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # -*- encoding: utf8 -*-
-from flask import Flask, request
+from flask import Flask, request, make_response
 
 app = Flask(__name__)
 
@@ -13,6 +13,21 @@ def index():
 @app.route('/user/<name>')
 def show_user_name(name):
     return '<h1>Hello, {}!</h1>'.format(name)
+
+
+@app.route('/bad-request')
+def bad_request_example():
+    return '<h1>Bad request</h1', 400
+
+
+@app.route('/cookies')
+def cookies_demo():
+    response = make_response(
+        '<h1>this docs carries a cookie</h1>')
+    response.set_cookie('answer', '42')
+    response.content_type = "application/json"
+    response.set_data('teste')
+    return response
 
 
 if __name__ == '__main__':
