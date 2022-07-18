7d4ac02f1fb952fee31923c18ed5c0432484b517
Cristiano Alexandre de Faria <cristianoaf81@hotmail.com>
2022-07-18 14:29:22 -0300

    get started with wtf forms

--- app.py
+++ app.py
@@ -2,8 +2,15 @@
 from flask import Flask, request, make_response, redirect, abort, render_template
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 from datetime import datetime
 import os
+
+class NameForm(FlaskForm):
+    name = StringField('what is your name?',validators=[DataRequired()])
+    submit = SubmitField('Submit')
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
